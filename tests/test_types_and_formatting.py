e3acf265e59626e43a7a8c80b8b99189ccd632e7
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-06-22 13:37:16 +0200

    Remove old mypy cache in pytest

--- tests/test_types_and_formatting.py
+++ tests/test_types_and_formatting.py
@@ -1,7 +1,13 @@
 import os
+import shutil
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MYPY_CACHE_DIR = os.path.join(PROJECT_DIR, ".mypy_cache")
 
 
 def test_static_types():
+    if os.path.exists(MYPY_CACHE_DIR):
+        shutil.rmtree(MYPY_CACHE_DIR)
     exit_code = os.system("mypy tum_esm_signal")
     assert exit_code == 0
 
