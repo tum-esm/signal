c12b0e6fd5e8cc3eb4e6d69def2acf406514be5b
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-06-26 17:39:42 +0200

    Make layout customizable (5)
    add plotting order logic to plot grid

--- ui/src/components/custom/plot-grid.tsx
+++ ui/src/components/custom/plot-grid.tsx
@@ -3,7 +3,7 @@ import {
     LayoutCookieType,
 } from "@/utilities/layout-cookies";
 import { PlotPanel } from "./plot-panel";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import {
     TableColumnRecordType,
     fetchTableColumns,
@@ -21,6 +21,9 @@ export function PlotGrid(props: {
     refreshPeriod: -1 | 10 | 30 | 60;
 }) {
     const [columns, setColumns] = useState<TableColumnRecordType[] | undefined>(
+        undefined
+    );
+    const [layout, setLayout] = useState<LayoutCookieElementType | undefined>(
         undefined
     );
 
@@ -43,19 +46,39 @@ export function PlotGrid(props: {
         f();
     }, [props.pb, props.collectionName, props.tableName]);
 
-    const currentLayoutCookieElement: LayoutCookieElementType | undefined =
-        useMemo(() => {
+    useEffect(() => {
+        if (columns !== undefined) {
             const index = findIndex(
                 props.layoutCookie,
                 (lce) =>
                     lce.collectionName == props.collectionName &&
                     lce.tableName == props.tableName
             );
-            if (index === -1) {
-                return undefined;
+            if (index !== -1) {
+                const newLayout = props.layoutCookie[index];
+                if (
+                    newLayout.columns.map((c) => c.name).sort() ===
+                    columns.map((c) => c.columnName).sort()
+                ) {
+                    setLayout(newLayout);
+                    return;
+                }
+                console.log("Column names have changed, resetting layout.");
             }
-            return props.layoutCookie[index];
-        }, [props.layoutCookie, props.collectionName, props.tableName]);
+            // set default layout
+            setLayout({
+                collectionName: props.collectionName,
+                tableName: props.tableName,
+                columns: columns
+                    .sort((c1, c2) => (c1.columnName > c2.columnName ? 1 : -1))
+                    .map((c, i) => ({
+                        name: c.columnName,
+                        position: i,
+                        hidden: false,
+                    })),
+            });
+        }
+    }, [props.layoutCookie, props.collectionName, props.tableName, columns]);
 
     if (columns === undefined) {
         return (
@@ -64,16 +87,30 @@ export function PlotGrid(props: {
             </div>
         );
     }
-    if (currentLayoutCookieElement === undefined) {
+
+    if (layout === undefined) {
         return (
             <div className="mx-6 my-6 text-base font-semibold">
                 Loading layout...
             </div>
         );
     }
+
+    const renderedColumns: TableColumnRecordType[] = [];
+    layout.columns
+        .sort((c1, c2) => c1.position - c2.position)
+        .filter((c) => !c.hidden)
+        .forEach((c) => {
+            const index = findIndex(
+                columns,
+                (col) => col.columnName === c.name
+            );
+            renderedColumns.push(columns[index]);
+        });
+
     return (
         <div className={cn("grid grid-cols-1 4xl:grid-cols-2 gap-4")}>
-            {columns.map((c) => (
+            {renderedColumns.map((c) => (
                 <PlotPanel
                     key={c.id}
                     tableColumn={c}
